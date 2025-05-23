4200cf28e9bfecc83f786f09b8d3385df9633b31
Élvis Corrêa Miranda Júnior <elviscorreamirandajr@gmail.com>
2025-05-23 16:07:37 -0300

    :sparkles: feat(classes) Construtores atualizados e adicionadas as funções de calcular pontuação por posição

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,22 +1,23 @@
 import database.*;
+import java.sql.*;
 
 public class Jogador {
-    private static int nroJogadores = 0;
-
-    private final int id;
+    private int id;
     private String nome;
     private Posicao posicao;
     private Clube clube;
     private double preco;
     private double overall;
     private Stats stats;
+    private final DbFunctions db = new DbFunctions();
 
-    public Jogador(String nome, Posicao posicao, Clube clube, double preco) {
-        this.id = nroJogadores++;
+    public Jogador(int id, String nome, Posicao posicao, Clube clube, double preco, double overall) {
+        this.id = id;
         this.nome = nome;
         this.posicao = posicao;
         this.clube = clube;
         this.preco = preco;
+        this.overall = overall;
     }
 
     public String getNome() {
@@ -39,6 +40,15 @@ public class Jogador {
         return overall;
     }
 
+    public String getStringPosicao(){
+        return switch (posicao) {
+            case ATACANTE -> "ATACANTE";
+            case MEIA -> "MEIA";
+            case ZAGUEIRO -> "ZAGUEIRO";
+            case GOLEIRO -> "GOLEIRO";
+        };
+    }
+
 
     public Stats getStats() {
         return stats;
@@ -49,42 +59,3 @@ public class Jogador {
     }
 }
 
-public class Goleiro extends Jogador{
-    public Goleiro(String nome, Posicao posicao, Clube clube, double preco) {
-        super(nome, posicao, clube, preco);
-    }
-
-    public double calcularPontuacao(){
-        return 0;
-    }
-}
-
-public class Zagueiro extends Jogador{
-    public Zagueiro(String nome, Posicao posicao, Clube clube, double preco) {
-        super(nome, posicao, clube, preco);
-    }
-
-    public double calcularPontuacao(){
-        return 0;
-    }
-}
-
-public class Meia extends Jogador{
-    public Meia(String nome, Posicao posicao, Clube clube, double preco) {
-        super(nome, posicao, clube, preco);
-    }
-
-    public double calcularPontuacao(){
-        return 0;
-    }
-}
-
-public class Atacante extends Jogador{
-    public Atacante(String nome, Posicao posicao, Clube clube, double preco) {
-        super(nome, posicao, clube, preco);
-    }
-
-    public double calcularPontuacao(){
-        return 0;
-    }
-}
--- src/main/java/Stats.java
+++ src/main/java/Stats.java
@@ -7,8 +7,104 @@ public class Stats {
     private int defesas;
     private int defesaPenalti;
     private int golsContra;
-    private int cartaoVermelho;
+    private boolean cartaoVermelho;
     private int golsSofridos;
     private int cartaoAmarelo;
     private int faltasCometidas;
+
+    public int getDesarmes() {
+        return desarmes;
+    }
+
+    public void setDesarmes(int desarmes) {
+        this.desarmes = desarmes;
+    }
+
+    public int getGols() {
+        return gols;
+    }
+
+    public void setGols(int gols) {
+        this.gols = gols;
+    }
+
+    public int getAssistencias() {
+        return assistencias;
+    }
+
+    public void setAssistencias(int assistencias) {
+        this.assistencias = assistencias;
+    }
+
+    public boolean isSg() {
+        return sg;
+    }
+
+    public void setSg(boolean sg) {
+        this.sg = sg;
+    }
+
+    public int getFinalizacoes() {
+        return finalizacoes;
+    }
+
+    public void setFinalizacoes(int finalizacoes) {
+        this.finalizacoes = finalizacoes;
+    }
+
+    public int getDefesas() {
+        return defesas;
+    }
+
+    public void setDefesas(int defesas) {
+        this.defesas = defesas;
+    }
+
+    public int getDefesaPenalti() {
+        return defesaPenalti;
+    }
+
+    public void setDefesaPenalti(int defesaPenalti) {
+        this.defesaPenalti = defesaPenalti;
+    }
+
+    public int getGolsContra() {
+        return golsContra;
+    }
+
+    public void setGolsContra(int golsContra) {
+        this.golsContra = golsContra;
+    }
+
+    public boolean getCartaoVermelho() {
+        return cartaoVermelho;
+    }
+
+    public void setCartaoVermelho(boolean cartaoVermelho) {
+        this.cartaoVermelho = cartaoVermelho;
+    }
+
+    public int getGolsSofridos() {
+        return golsSofridos;
+    }
+
+    public void setGolsSofridos(int golsSofridos) {
+        this.golsSofridos = golsSofridos;
+    }
+
+    public int getCartaoAmarelo() {
+        return cartaoAmarelo;
+    }
+
+    public void setCartaoAmarelo(int cartaoAmarelo) {
+        this.cartaoAmarelo = cartaoAmarelo;
+    }
+
+    public int getFaltasCometidas() {
+        return faltasCometidas;
+    }
+
+    public void setFaltasCometidas(int faltasCometidas) {
+        this.faltasCometidas = faltasCometidas;
+    }
 }
