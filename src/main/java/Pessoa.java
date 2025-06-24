64d2cd6b00fe4148269013d523865d0683284fa6
gusfring41 <gusfring.a@gmail.com>
2025-06-24 15:47:03 -0300

    feat: tabela usuário, getter e setter da classe abstrata

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -12,6 +12,30 @@ public abstract class Pessoa {
         this.senha = senha;
         this.id = nroPessoas++;
     }
+
+    public static int getNroPessoas() {
+        return nroPessoas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
 
 class Admin extends Pessoa {
